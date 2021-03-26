9633f2e41bc1a106ffdccf3836972ce1af1e5de1
thesagarmahajan <sagarmahajan31997@gmail.com>
2021-03-26 22:10:43 +0530

    First Commit

---
+++ controllers/raw_ctrl.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+
+let userSchema = new mongoose.Schema({
+    full_name:String,
+    email:String,
+    phone:String,
+    password:String
+});
+
+const userModel = mongoose.model('user', userSchema, 'users');
+
+const constr = "mongodb://localhost:27017/shamProject";
+
+mongoose.connect(constr, {useNewUrlParser: true, useUnifiedTopology: true});
+
+router.use(bodyparser.json());
+
+router.post("/createUser", function(req, res){
+    var user = new userModel();
+    // console.log(req.body);
+    // res.status(200).json({"status_message":"HI"});
+    for(key in req.body){
+        user[key] = req.body[key];
+    }
+    
+    user.save(function(err, data){
+        if(err){
+            res.status(500).json({"status_message":err.toString()});
+            console.log(err);
+        }
+        else{
+            res.status(200).json({"status_message":"user_created_successfully"});
+        }
+    });
+});
+
+router.get("/getAllUsers", function(req,res){
+    userModel.find(function(err,data){
+        if(err){
+            res.status(500).json({"status_message":err.toString()});
+            console.log(err);
+        }
+        else{
+            res.status(200).json({"payload":data});
+        }
+    });
+});
+
+module.exports= router;
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+var app = express();
+// const urlencoded = require('./controllers/urlencoded_ctrl.js');
+// const raw = require('./controllers/raw_ctrl.js');
+const formdata = require('./controllers/formdata_ctrl.js');
+const all_routes = require('./controllers');
+
+app.listen(5050, function(){});
+// app.use("/users", urlencoded);
+// app.use("/users", raw);
+app.use("/users", formdata);
+app.use("/", all_routes);
